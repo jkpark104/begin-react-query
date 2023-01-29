30e44c111c42cec007869d6439bcf09ca0b33b4f
jkpark104 <jkrang104@gmail.com>
2023-01-29 21:14:43 +0900

    update learnings

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery, useMutation } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -22,13 +24,53 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery(
+    ["comments", post.id],
+    () => fetchComments(post.id)
+  );
+
+  const {
+    isError: isDeleteError,
+    isLoading: isDeleteLoading,
+    isSuccess: isDeleteSuccess,
+    mutate: deleteMutate,
+  } = useMutation((postId) => deletePost(postId));
+
+  const {
+    isError: isUpdateError,
+    isLoading: isUpdateLoading,
+    isSuccess: isUpdateSuccess,
+    mutate: updateMutate,
+  } = useMutation((postId) => updatePost(postId));
+
+  if (isLoading) {
+    return <h3>Loading!</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Error</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutate(post.id)}>Delete</button>
+      <button onClick={() => updateMutate(post.id)}>Update title</button>
+      {isDeleteError && <p style={{ color: "red" }}>Error deleting the post</p>}
+      {isDeleteLoading && <p style={{ color: "purple" }}>Deleting the post</p>}
+      {isDeleteSuccess && (
+        <p style={{ color: "green" }}>Post has (not) been deleted</p>
+      )}
+      {isUpdateError && <p style={{ color: "red" }}>Error updating the post</p>}
+      {isUpdateLoading && <p style={{ color: "purple" }}>Updating the post</p>}
+      {isUpdateSuccess && (
+        <p style={{ color: "green" }}>Post has (not) been updated</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
